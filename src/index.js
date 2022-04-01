9774f10ce88abb55a54091c4be3d10de250d7da4
Yehya <40221838+BalderasBa@users.noreply.github.com>
2022-04-01 19:59:26 +0100

    routing

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
-import { render } from 'react-dom'
-import App from './App'
-import { ThemeProvider } from './contexts/theme'
-import './index.css'
+import { render } from "react-dom";
+import App from "./App";
+import { ThemeProvider } from "./contexts/theme";
+import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 render(
   <ThemeProvider>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/index" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </ThemeProvider>,
-  document.getElementById('root')
-)
+  document.getElementById("root")
+);
